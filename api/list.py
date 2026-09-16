7116308b866cf2629221c9c86dcfca256e1519f6
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2026-09-17 02:27:32 +0530

    retrying temporary change for test

--- api/list.py
+++ api/list.py
@@ -1,1 +1,15 @@
-[1,2,3]
+from http.server import BaseHTTPRequestHandler
+import json
+
+HUGE_LIST = sorted(["apple", "zebra", "banana", "mango", "cherry"] * 20000)
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+        response_payload = json.dumps({"data": HUGE_LIST})
+        self.wfile.write(response_payload.encode('utf-8'))
+        return
